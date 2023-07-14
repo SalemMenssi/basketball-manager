2b9ed1928aa6bf924f7c926d67ca6248dc2cf0a2
Salem <selem.menssi.2000@gmail.com>
2023-07-14 13:59:43 +0100

    ui

---
+++ Assets/Doozy/Runtime/UIManager/Ids/UIViewIdExtension.cs
@@ -0,0 +1,38 @@
+// Copyright (c) 2015 - 2023 Doozy Entertainment. All Rights Reserved.
+// This code can only be used under the standard Unity Asset Store End User License Agreement
+// A Copy of the EULA APPENDIX 1 is available at http://unity3d.com/company/legal/as_terms
+
+//.........................
+//.....Generated Class.....
+//.........................
+//.......Do not edit.......
+//.........................
+
+using System.Collections.Generic;
+// ReSharper disable All
+namespace Doozy.Runtime.UIManager.Containers
+{
+    public partial class UIView
+    {
+        public static IEnumerable<UIView> GetViews(UIViewId.ui id) => GetViews(nameof(UIViewId.ui), id.ToString());
+        public static void Show(UIViewId.ui id, bool instant = false) => Show(nameof(UIViewId.ui), id.ToString(), instant);
+        public static void Hide(UIViewId.ui id, bool instant = false) => Hide(nameof(UIViewId.ui), id.ToString(), instant);
+    }
+}
+
+namespace Doozy.Runtime.UIManager
+{
+    public partial class UIViewId
+    {
+        public enum ui
+        {
+            mainMenu,
+            match,
+            setting,
+            startMatch,
+            store,
+            team,
+            trade
+        }    
+    }
+}
